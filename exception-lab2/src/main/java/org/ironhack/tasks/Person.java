c520ac5ac4c606b2dfba3a85620cc834a0155da1
Mansura <badalovamansura40@gmail.com>
2026-02-27 02:58:56 +0400

    Person created

---
+++ exception-lab2/src/main/java/org/ironhack/tasks/Person.java
@@ -0,0 +1,72 @@
+package org.ironhack.tasks;
+
+import java.util.Objects;
+
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String occupation;
+
+    public Person(int id, String name, int age, String occupation) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age cannot be less than 0");
+        }
+        this.age = age;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(occupation, person.occupation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, occupation);
+    }
+
+    @Override public String toString() {
+    return "Person{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", age=" + age +
+            ", occupation='" + occupation + '\'' +
+            '}';
+}}
